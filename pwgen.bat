bc81a351ae4315db4367a774fc988fcc57e8e7ff
arsenyratner <arsenyratner@gmail.com>
2023-05-04 12:05:45 +0300

    pwgen.bat 1

---
+++ pwgen.bat
@@ -0,0 +1,10 @@
+@echo off
+::workdir=%ProgramFiles%\cmdutils
+set APG=%ProgramFiles%\cmdutils\apg.exe
+set OPTIONS=-n5 -EOI10l -a0 -MNCLs -c cl_seed
+set PWLEN=8
+"%APG%" %OPTIONS% -m %PWLEN% -x %PWLEN% 
+set PWLEN=10
+"%APG%" %OPTIONS% -m %PWLEN% -x %PWLEN% 
+set PWLEN=15
+"%APG%" %OPTIONS% -m %PWLEN% -x %PWLEN% 
